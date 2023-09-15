c023444b16aef4ef19dbd8125b7f122d8ac7417a
lhurdman <hur21012@byui.edu>
2023-09-15 17:05:54 -0600

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What is your grade? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letterGrade = "";
+
+        if (percent >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (percent >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (percent >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (percent >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is a {letterGrade}");
+        
+        
     }
 }
