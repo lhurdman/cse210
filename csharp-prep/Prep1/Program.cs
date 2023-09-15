38458d54fbe2a280ef2407e105f28ef4b28d6df3
lhurdman <hur21012@byui.edu>
2023-09-15 16:24:46 -0600

    Yeahhhhh

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("This is in C#");
+
+        // Console.WriteLine("What is your favorite color? ");
+        // string color = Console.ReadLine();
+        // Console.WriteLine($"Your color is {color}");
+
+        Console.WriteLine("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.WriteLine("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
