cff9b5ae6b205569253a833bfdf0c4ab849151c7
lhurdman <hur21012@byui.edu>
2023-09-21 16:08:00 -0600

    Prep 3 Loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("What is the madgic number? ");
+        // int madgicNumber = int.Parse(Console.ReadLine()):
+
+        Random randomGenerator = new Random();
+        int madgicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != madgicNumber) {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (madgicNumber > guess){
+                Console.WriteLine("Higher");
+            }
+            else if (madgicNumber < guess){
+                Console.WriteLine("Lower");
+            }
+            else {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
