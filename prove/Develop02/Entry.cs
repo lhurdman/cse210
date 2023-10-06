c91693c8b1a1b7177a09f15a0f37ace80781bf57
lhurdman <hur21012@byui.edu>
2023-10-06 11:20:58 -0600

    commit

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,11 +1,17 @@
-    
+using System;
+
 namespace MyEntry
 {
     public class Entry
     {
-        public string _words;
-        public string _prompts;
-        public int _date;
-        public string _entry;
+        public string Prompt {get; set;}
+        public string Entry1 {get; set;}
+        public int Date {get; set;}
+
+        // Who was the most interesting person I interacted with today?
+        // What was the best part of my day?
+        // How did I see the hand of the Lord in my life today?
+        // What was the strongest emotion I felt today?
+        // If I had one thing I could do over today, what would it be?
     }
 }
--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,5 +1,8 @@
-//using System;
-
+using System;
+static void Main(string[] args)
+{
+    
+}
 namespace MyJournal
 {
 
