6a0eb1ea776a541aa30db0eb1e86f4cc7005849f
lhurdman <hur21012@byui.edu>
2023-10-18 11:26:09 -0600

    added lh

---
+++ prove/Develop03/reference.cs
@@ -0,0 +1,25 @@
+using System;
+
+/*
+W06 Prove: Developer 
+Purpose: Stores the book, chapter, and verse.
+Name: Lindsay Hurdman
+(c) 2023
+*/
+
+class lhReference
+{
+    private string _lhbook;
+    private string _lhchapter;
+    private string _lhverse;
+
+    public lhReference(_book, _chapter, _verse)
+    {
+        lhReference = _book, _chapter, _verse;
+    }
+
+    public lhReference(lhReference)
+    {
+        string lhReference;
+    }
+}
