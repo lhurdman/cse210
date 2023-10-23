faab17ad4bd5f92d8e7cdaa233508b970da278d8
lhurdman <hur21012@byui.edu>
2023-10-23 12:19:20 -0600

    finished

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment m1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(m1.GetSummary());
+        Console.WriteLine(m1.GetHomeworkList());
+
+
+        WritingAssignment w1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine(w1.GetSummary());
+        Console.WriteLine(w1.GetWritingInformation());
+
     }
 }
